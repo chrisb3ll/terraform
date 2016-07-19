70abd690e8f06728a07fe2d6b914a9dc48bf9cbf
Chris Bell <Chris.Bell@sainsburys.co.uk>
2016-07-20 00:46:30 +0100

    add remote state

--- tf.sh
+++ tf.sh
@@ -74,6 +74,26 @@ else
   role="roles/shared"
 fi
 
+# retrieve account ID
+account_id=$(aws iam get-user | awk '/arn:aws:/{print $2}' | awk -F ':' '{print $5}')
+
+# remote state
+account_alias=$(aws iam list-account-aliases | awk -F'"' 'FNR == 3 {print $2}')
+bucket="${account_alias:-${account_id}}-terraform-state"
+
+log "Ensure S3 bucket exist: ${bucket}"
+(aws s3 ls s3://${bucket} --region ${AWS_DEFAULT_REGION}) > /dev/null 2>&1 || (aws s3 mb s3://${bucket} --region ${AWS_DEFAULT_REGION})
+
+log "Enabling remote state"
+(terraform remote config \
+    -backend=s3 -backend-config="bucket=${bucket}" \
+    -backend-config="key=${env}.tfstate" \
+    -backend-config="region=${AWS_DEFAULT_REGION}"
+    ) || die "Failed to initialize terraform remote state configuration"
+
+log "Retrieving remote state"
+terraform remote pull || die "Failed to pull the terraform remote state"
+
 # main
 terraform get $role || die 'Failed to terraform get'
 terraform $action \
